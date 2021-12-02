38322a3b73e02790f80ae083652e863a601c4ff3
tangxiangong <tangxg16@gmail.com>
2021-12-02 18:46:57 +0800

    add figures

--- source/multiple_states.py
+++ source/multiple_states.py
@@ -5,7 +5,14 @@ import numpy as np
 from numpy import random
 import matplotlib.pyplot as plt
 from trajectory import Trajectory
-from rnd import n_rand, skewed_stable_rnd
+from rnd import n_rand, skewed_stable_rnd, power_rnd
+
+
+def rnd(alpha):
+    if alpha == 1:
+        return random.exponential(1)
+    else:
+        return power_rnd(alpha)
 
 
 class FCP(Trajectory):
@@ -48,16 +55,67 @@ class FCP(Trajectory):
         plt.show()
 
 
+class LW(Trajectory):
+    def __init__(self, t_len, v_waiting,  v_velocity, transition_matrix, init_state, init_position=0):
+        super(Trajectory, self).__init__()
+        self._T = t_len
+        self._I = np.array(init_state)
+        self._M = np.array(transition_matrix)
+        self._alpha = np.array(v_waiting)
+        self._v = np.array(v_velocity)
+        self._x0 = init_position
+        self._simulate()
+
+    def _simulate(self):
+        self._t = np.zeros(1)
+        self._x = np.array([self._x0])
+        total_time = 0
+        n = 1
+        init = n_rand(self._I)
+        current_state = init
+        while True:
+            tau = rnd(self._alpha[current_state])
+            v0 = self._v[current_state]
+            if random.random() < 0.5:
+                d = -1
+            else:
+                d = 1
+            if total_time + tau > self._T:
+                temp_t = self._T - total_time
+                self._t = np.append(self._t, self._T)
+                self._x = np.append(self._x, self._x[-1]+d*v0*temp_t)
+                self._n = n + 1
+                break
+            else:
+                total_time += tau
+                self._t = np.append(self._t, total_time)
+                self._x = np.append(self._x, self._x[-1] + d * v0 * tau)
+                n += 1
+                next_state = n_rand(self._M[current_state])
+                current_state = next_state
+
+    def plot(self):
+        plt.figure()
+        plt.plot(self._t, self._x)
+        plt.show()
+
+
 if __name__ == "__main__":
-    a = [1/3, 1/3, 1/3]
-    M = [[0.3, 0.5, 0.2],
-         [0.5, 0.1, 0.4],
-         [0.2, 0.5, 0.3]]
-    b = [0.5, 0.43, 0.6]
-    model = FCP(100, b, M, a)
+    init = [1/3, 1/3, 1/3]
+    M = [[1/3, 1/3, 1/3],
+         [1/4, 1/2, 1/4],
+         [1/6, 1/3, 1/2]]
+    v = [1, 3, 5]
+    a = [0.7, 1, 1.6]
+    # M = [[0.3, 0.5, 0.2],
+    #      [0.5, 0.1, 0.4],
+    #      [0.2, 0.5, 0.3]]
+    # b = [0.5, 0.43, 0.6]
+    model = LW(100, a, v, M, init_state=init)
     t,x = model.get()
     fig = plt.figure()
-    plt.step(t, x)
+    plt.plot(t, x)
     plt.xlabel("t")
     plt.ylabel("x")
     fig.savefig("fcp.eps")
+    # plt.show()
